232dd87979147b2e7e191f61b2404ba77d708c85
Sreehari P Shaiju <133079958+Sreeharips1@users.noreply.github.com>
2025-03-29 00:41:18 +0530

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -41,7 +41,7 @@ const LoginPage = () => {
 
             setLoading(true);
             try {
-                const response = await fetch('http://localhost:5000/api/auth/handlelogin', {
+                const response = await fetch('https://userbackend-1.onrender.com/api/auth/handlelogin', {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify({ email }),
